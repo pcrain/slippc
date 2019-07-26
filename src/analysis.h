5423d62f7b1b2c638e1fbbe81b70397a19141485
Patrick Crain <patrickacrain@gmail.com>
2019-07-26 13:54:17 -0500

    Fixed uninitialized memory in analysis

--- src/analysis.h
+++ src/analysis.h
@@ -16,67 +16,68 @@ namespace slip {
 
 //Struct for storing basic punish infomations
 struct Punish {
-  unsigned num_moves = 0;  //WARNING: need to initialize this to avoid some awful OOB memory accesses
-  unsigned start_frame;
-  unsigned end_frame;
-  float    start_pct;
-  float    end_pct;
-  uint8_t  last_move_id;
-  int      opening;   // TODO: make an enum for this
-  int      kill_dir;  //-1 = no kill, other directions as specified in Dir enum
+  unsigned num_moves    = 0;
+  unsigned start_frame  = 0;
+  unsigned end_frame    = 0;
+  float    start_pct    = 0;
+  float    end_pct      = 0;
+  uint8_t  last_move_id = 0;
+  int      opening      = 0;  // TODO: make an enum for this
+  int      kill_dir     = 0;  //-1 = no kill, other directions as specified in Dir enum
 };
 
 //Struct for holding analysis data for a particular player within a game
 struct AnalysisPlayer {
-  unsigned     port;
-  std::string  tag_player;
-  std::string  tag_css;
-  unsigned     char_id;
-  std::string  char_name;
-  unsigned     airdodges;
-  unsigned     spotdodges;
-  unsigned     rolls;
-  unsigned     dashdances;
-  unsigned     l_cancels_hit;
-  unsigned     l_cancels_missed;
-  unsigned     techs;
-  unsigned     walltechs;
-  unsigned     walljumps;
-  unsigned     walltechjumps;
-  unsigned     missed_techs;
-  unsigned     ledge_grabs;
-  unsigned     air_frames;
-  unsigned     end_stocks;
-  unsigned     end_pct;
-  unsigned     wavedashes;
-  unsigned     wavelands;
-  unsigned     neutral_wins;
-  unsigned     pokes;
-  unsigned     counters;
-  unsigned     dyn_counts[Dynamic::__LAST];  //Frame counts for player interaction dynamics
+  unsigned     port = 0;
+  std::string  tag_player       = "";
+  std::string  tag_css          = "";
+  unsigned     char_id          = 0;
+  std::string  char_name        = "";
+  unsigned     airdodges        = 0;
+  unsigned     spotdodges       = 0;
+  unsigned     rolls            = 0;
+  unsigned     dashdances       = 0;
+  unsigned     l_cancels_hit    = 0;
+  unsigned     l_cancels_missed = 0;
+  unsigned     techs            = 0;
+  unsigned     walltechs        = 0;
+  unsigned     walljumps        = 0;
+  unsigned     walltechjumps    = 0;
+  unsigned     missed_techs     = 0;
+  unsigned     ledge_grabs      = 0;
+  unsigned     air_frames       = 0;
+  unsigned     end_stocks       = 0;
+  unsigned     end_pct          = 0;
+  unsigned     wavedashes       = 0;
+  unsigned     wavelands        = 0;
+  unsigned     neutral_wins     = 0;
+  unsigned     pokes            = 0;
+  unsigned     counters         = 0;
+  unsigned*    dyn_counts;  //Frame counts for player interaction dynamics
   Punish*      punishes;
 
   AnalysisPlayer() {
-    punishes = new Punish[MAX_PUNISHES];
+    dyn_counts = new unsigned[Dynamic::__LAST]{0};
+    punishes   = new Punish[MAX_PUNISHES];
   }
   ~AnalysisPlayer() {
+    delete dyn_counts;
     delete punishes;
   }
 };
 
 //Struct for holding all analysis data within a game
 struct Analysis {
-  std::string     game_time;
-  std::string     slippi_version;
-  std::string     parser_version;
-  std::string     analyzer_version;
-  std::string     stage_name;
-  unsigned        stage_id;
-  unsigned        winner_port;
-  unsigned        game_length;
+  bool           success           = false;
+  std::string     game_time        = "";
+  std::string     slippi_version   = "";
+  std::string     parser_version   = "";
+  std::string     analyzer_version = "";
+  std::string     stage_name       = "";
+  unsigned        stage_id         = 0;
+  unsigned        winner_port      = 0;
+  unsigned        game_length      = 0;
   AnalysisPlayer* ap;
-
-  bool           success;
   unsigned*      dynamics;  //Interaction dynamics on a per_frame basis
 
   Analysis() {
