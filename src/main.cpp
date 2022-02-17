e925a92ddf835fbfcb752277e788a552fb9dd8ac
Patrick Crain <patrickacrain@gmail.com>
2022-02-17 09:57:34 -0600

    refactored main code

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,17 @@
 #include "analyzer.h"
 #include "compressor.h"
 
+// #define GUI_ENABLED 1  //debug, normally enable this from the makefile
+
 #if GUI_ENABLED == 1
   #include "portable-file-dialogs.h"
 #endif
 
 namespace slip {
 
-typedef std::vector<std::__cxx11::basic_string<char> > str_vec;
+#if GUI_ENABLED == 1
+  typedef std::vector<std::__cxx11::basic_string<char> > str_vec;
+#endif
 
 // https://stackoverflow.com/questions/865668/how-to-parse-command-line-arguments-in-c
 char* getCmdOption(char ** begin, char ** end, const std::string & option) {
@@ -51,167 +55,193 @@ void printUsage() {
     ;
 }
 
-int run(int argc, char** argv) {
-  if (cmdOptionExists(argv, argv+argc, "-h")) {
-    printUsage();
-    return 0;
-  }
+typedef struct _cmdoptions {
+  char* dlevel       = nullptr;
+  char* infile       = nullptr;
+  char* cfile        = nullptr;
+  char* outfile      = nullptr;
+  char* analysisfile = nullptr;
+  bool  nodelta      = false;
+  bool  encode       = false;
+  bool  rawencode    = false;
+  bool  skipsave     = false;
+  bool  dumpgecko    = false;
+  int   debug        = 0;
+} cmdoptions;
 
-  int debug          = 0;
-  char* dlevel       = getCmdOption(   argv, argv+argc, "-d");
-  char* infile       = getCmdOption(   argv, argv+argc, "-i");
-  char* cfile        = getCmdOption(   argv, argv+argc, "-X");
-  char* outfile      = getCmdOption(   argv, argv+argc, "-j");
-  char* analysisfile = getCmdOption(   argv, argv+argc, "-a");
-  bool  nodelta      = cmdOptionExists(argv, argv+argc, "-f");
-  bool  encode       = cmdOptionExists(argv, argv+argc, "-x");
-  bool  rawencode    = cmdOptionExists(argv, argv+argc, "--raw-enc");
-  bool  skipsave     = cmdOptionExists(argv, argv+argc, "--skip-save");
-  bool  dumpgecko    = cmdOptionExists(argv, argv+argc, "--dump-gecko");
+cmdoptions getCommandLineOptions(int argc, char** argv) {
+  _cmdoptions c;
+  c.dlevel       = getCmdOption(   argv, argv+argc, "-d");
+  c.infile       = getCmdOption(   argv, argv+argc, "-i");
+  c.cfile        = getCmdOption(   argv, argv+argc, "-X");
+  c.outfile      = getCmdOption(   argv, argv+argc, "-j");
+  c.analysisfile = getCmdOption(   argv, argv+argc, "-a");
+  c.nodelta      = cmdOptionExists(argv, argv+argc, "-f");
+  c.encode       = cmdOptionExists(argv, argv+argc, "-x");
+  c.rawencode    = cmdOptionExists(argv, argv+argc, "--raw-enc");
+  c.skipsave     = cmdOptionExists(argv, argv+argc, "--skip-save");
+  c.dumpgecko    = cmdOptionExists(argv, argv+argc, "--dump-gecko");
 
-  if (dlevel) {
-    if (dlevel[0] >= '0' && dlevel[0] <= '9') {
-      debug = dlevel[0]-'0';
+  if (c.dlevel) {
+    if (c.dlevel[0] >= '0' && c.dlevel[0] <= '9') {
+      c.debug = c.dlevel[0]-'0';
     } else {
       std::cerr << "Warning: invalid debug level" << std::endl;
     }
   }
-  if (debug) {
-    std::cout << "Running at debug level " << +debug << std::endl;
+  if (c.debug) {
+    std::cerr << "Running at debug level " << +c.debug << std::endl;
   }
 
-  if (not infile) { //Running in GUI dialog mode
+  return c;
+}
+
+#if GUI_ENABLED == 1
+  void getGUIOptions(cmdoptions &c) {
+    if (_debug < 1) {
+      _debug = 1;  //force some debug output
+    }
     std::string save, saveext;
-    #if GUI_ENABLED == 1
-      debug = 1;  //Force some debug output in GUI mode
-      str_vec file_res = pfd::open_file(
-        "Select an input File", ".", {"Slippi Files", "*.slp *.zlp"}).result();
-      if(file_res.empty()) {
-        printUsage();
-        return -1;
-      }
-      stringtoChars(file_res[0],&infile);
-
-      std::string inbase = getFileBase(file_res[0]);
-      std::string inext  = getFileExt(file_res[0]);
-
-      if (inext.compare("slp") == 0) {
-        if (askYesNo("Compress?","Output compressed file (yes) or JSON (no)?")) {
-          std::cout << "GUI mode, compressed output" << std::endl;
-          save = pfd::save_file("Select an Output file", inbase+".zlp", { "Zlippi Files", "*.zlp"}).result();
-          stringtoChars(save,&cfile);
-        } else if (askYesNo("Analysis?", "Output analysis JSON (yes) or regular JSON (no)?")) {
-          std::cout << "GUI mode, analysis output" << std::endl;
-          save = pfd::save_file("Select an Output file", inbase+".json", { "JSON Files", "*.json"}).result();
-          stringtoChars(save,&analysisfile);
-        } else {
-          std::cout << "GUI mode, JSON output" << std::endl;
-          save = pfd::save_file("Select an Output file", inbase+".json", { "JSON Files", "*.json"}).result();
-          stringtoChars(save,&outfile);
-        }
-      } else if (inext.compare("zlp") == 0) {
-        std::cout << "GUI mode, decompressed output" << std::endl;
-        save = pfd::save_file("Select an Output file", inbase+".slp", { "Slippi Files", "*.slp"}).result();
-        stringtoChars(save,&cfile);
-      }
-    #endif
-    if(save.empty()) {
-      std::cerr << "No output file selected" << std::endl;
+    str_vec file_res = pfd::open_file(
+      "Select an input File", ".", {"Slippi Files", "*.slp *.zlp"}).result();
+    if(file_res.empty()) {
       printUsage();
-      return -1;
+      return "";
+    }
+    stringtoChars(file_res[0],&c.infile);
+
+    std::string inbase = getFileBase(file_res[0]);
+    std::string inext  = getFileExt(file_res[0]);
+
+    if (inext.compare("slp") == 0) {
+      if (askYesNo("Compress?","Output compressed file (yes) or JSON (no)?")) {
+        std::cerr << "GUI mode, compressed output" << std::endl;
+        save = pfd::save_file("Select an Output file", inbase+".zlp", { "Zlippi Files", "*.zlp"}).result();
+        stringtoChars(save,&c.cfile);
+      } else if (askYesNo("Analysis?", "Output analysis JSON (yes) or regular JSON (no)?")) {
+        std::cerr << "GUI mode, analysis output" << std::endl;
+        save = pfd::save_file("Select an Output file", inbase+".json", { "JSON Files", "*.json"}).result();
+        stringtoChars(save,&c.analysisfile);
+      } else {
+        std::cerr << "GUI mode, JSON output" << std::endl;
+        save = pfd::save_file("Select an Output file", inbase+".json", { "JSON Files", "*.json"}).result();
+        stringtoChars(save,&c.outfile);
+      }
+    } else if (inext.compare("zlp") == 0) {
+      std::cerr << "GUI mode, decompressed output" << std::endl;
+      save = pfd::save_file("Select an Output file", inbase+".slp", { "Slippi Files", "*.slp"}).result();
+      stringtoChars(save,&c.cfile);
     }
   }
+#endif
 
-  if(cfile || encode) {
-    slip::Compressor *c = new slip::Compressor(debug);
+int parseSingleFile(const cmdoptions &c, const int debug) {
+  if(c.cfile || c.encode) {
+    slip::Compressor cmp(debug);
 
-    if (cfile) {
-      if (!(c->setOutputFilename(cfile))) {
-        std::cerr << "File " << cfile << " already exists or is invalid" << std::endl;
+    if (c.cfile) {
+      if (!(cmp.setOutputFilename(c.cfile))) {
+        std::cerr << "File " << c.cfile << " already exists or is invalid" << std::endl;
         return 4;
       }
     }
 
-    if (dumpgecko) {
-      std::cout << "setting gecko output filename" << std::endl;
-      c->setGeckoOutputFilename(infile);
+    if (c.dumpgecko) {
+      std::cerr << "setting gecko output filename" << std::endl;
+      cmp.setGeckoOutputFilename(c.infile);
     }
 
     std::cerr << "Encoding / decoding replay" << std::endl;
-    if (not c->loadFromFile(infile)) {
+    if (not cmp.loadFromFile(c.infile)) {
       std::cerr << "Failed to encode input; exiting" << std::endl;
-      delete c;
       return 2;
     }
-    if (rawencode) {
-        std::cerr << "Skipping validation" << std::endl;
-    } else {
-      std::cerr << "Validating encoding" << std::endl;
-      if (!c->validate()) {
-        std::cerr << "Validation failed; exiting" << std::endl;
-        delete c;
-        return 3;
-      }
+
+    std::cerr << "Validating encoding" << std::endl;
+    if (!cmp.validate()) {
+      std::cerr << "Validation failed; exiting" << std::endl;
+      return 3;
     }
-    if (skipsave) {
+
+    if (c.skipsave) {
       std::cerr << "Skipping saving" << std::endl;
     } else {
       std::cerr << "Saving encoded / decoded replay" << std::endl;
-      c->saveToFile(rawencode);
+      cmp.saveToFile(c.rawencode);
     }
     std::cerr << "Saved!" << std::endl;
-    delete c;
     return 0;
   }
 
-  slip::Parser *p = new slip::Parser(debug);
-  if (not p->load(infile)) {
+  slip::Parser p(debug);
+  if (not p.load(c.infile)) {
     std::cerr << "Could not load input; exiting" << std::endl;
-    delete p;
     return 2;
   }
 
-  if (outfile) {
-    if (outfile[0] == '-' && outfile[1] == '\0') {
+  if (c.outfile) {
+    if (c.outfile[0] == '-' && c.outfile[1] == '\0') {
       if (debug) {
         std::cerr << "Writing Slippi JSON data to stdout" << std::endl;
       }
-      std::cout << p->asJson(!nodelta) << std::endl;
+      std::cout << p.asJson(!c.nodelta) << std::endl;
     } else {
       if (debug) {
-        std::cout << "Saving Slippi JSON data to file" << std::endl;
+        std::cerr << "Saving Slippi JSON data to file" << std::endl;
       }
-      p->save(outfile,!nodelta);
+      p.save(c.outfile,!c.nodelta);
     }
   }
 
-  if (analysisfile) {
-    slip::Analysis *a  = p->analyze();
+  if (c.analysisfile) {
+    slip::Analysis *a  = p.analyze();
     if (a->success) {
-      if (analysisfile[0] == '-' && analysisfile[1] == '\0') {
+      if (c.analysisfile[0] == '-' && c.analysisfile[1] == '\0') {
         if (debug) {
-          std::cout << "Writing analysis to stdout" << std::endl;
+          std::cerr << "Writing analysis to stdout" << std::endl;
         }
         std::cout << a->asJson() << std::endl;
       } else {
         if (debug) {
-          std::cout << "Saving analysis to file" << std::endl;
+          std::cerr << "Saving analysis to file" << std::endl;
         }
-        a->save(analysisfile);
+        a->save(c.analysisfile);
       }
     }
     if (debug) {
-      std::cout << "Deleting analysis" << std::endl;
+      std::cerr << "Deleting analysis" << std::endl;
     }
 
     delete a;
   }
 
   if (debug) {
-    std::cout << "Cleaning up" << std::endl;
+    std::cerr << "Cleaning up" << std::endl;
   }
-  delete p;
   return 0;
+}
+
+int run(int argc, char** argv) {
+  if (cmdOptionExists(argv, argv+argc, "-h")) {
+    printUsage();
+    return 0;
+  }
+
+  cmdoptions c = getCommandLineOptions(argc,argv);
+
+  #if GUI_ENABLED == 1
+    if (not c.infile) { //if we don't have an input file, open file selector
+      getGUIOptions(c);
+    }
+  #endif
+
+  if (not c.infile) { //if we still don't have an input file, exit
+    std::cerr << "No output file selected" << std::endl;
+    printUsage();
+    return -1;
+  }
+
+  return parseSingleFile(c,c.debug);
 }
 
 }
