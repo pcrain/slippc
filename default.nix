de5aba94d9cf9d0a31780231eb716fdc90cbcbb5
Armando Ramirez <armando.m.ramirez@gmail.com>
2024-12-02 21:37:43 -0800

    buildInputs - not buildDepends

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ in { stdenv ? pkgs.stdenv, xz ? pkgs.xz }:
 stdenv.mkDerivation {
   name = "slippc";
   src = ./.;
-  buildDepends = [ xz ];
+  buildInputs = [ xz ];
 
   installPhase = ''
     mkdir -p $out/bin
